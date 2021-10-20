a5f2ef1c9b30c2773f8a12a3cc8a19febabfa506
shubhamsharma1996 <shubhamsen216@gmail.com>
2021-10-21 02:16:25 +0530

    validation for contact has been fixed

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -12,35 +12,59 @@ const Contact = () => {
       message:''
   })
   const [successMessage,setSuccessMessage] = useState('')
+  const [errorMessage,setErrorMessage] = useState('')
 
   const {name,email,contact,company,message} = state
 
   const handleData = (e) =>{
       setstate({...state,[e.target.name]:e.target.value})
   }
-
+  const msgs = (msg) =>{
+      setErrorMessage(msg)
+      setTimeout(()=>{
+        setErrorMessage('')
+    },3000)
+  }
 //   api call for submitting data in google sheets 
   const submitData = async(e) =>{
       e.preventDefault();
-      try{
-        const response = await fetch(
-            "https://v1.nocodeapi.com/blockreate/google_sheets/sXasARFzuWyJEckF?tabId=Sheet1",{
-                method:'POST',
-                headers:{
-                    'Content-Type' :'application/json'
-                },
-                body: JSON.stringify([[name,email,contact,company,message,new Date().toLocaleString()]])
-            })
-            await response.json()
-            window.open( "https://blockreate.com/Docs/Blockreate%20Pitch%20Deck.pdf", "_blank");
-            setstate({...state,name:'',email:'',contact:'',company:'',message:''})
-            setSuccessMessage('Thank You!! We have recieved your query, Look for our email in this next 24 Business Hours')
-            setTimeout(()=>{
-                setSuccessMessage('')
-            },3000)
-      }catch (err){
-        console.log(err)
+      const number = /^\d+$/
+      const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      if(!re.test(email)){
+          return msgs("Please enter valid email address")
       }
+      if(!number.test(contact)){
+        return  msgs("Please enter number values only in contact") 
+      }
+      
+      if(name === "" || contact === "" || email === "" || company === "" || message==='' ){
+        setErrorMessage('Please Fill All The Field')
+             setTimeout(()=>{
+                setErrorMessage('')
+            },3000)
+      }else{
+
+          try{
+              const response = await fetch(
+            "https://v1.nocodeapi.com/blockreate/google_sheets/sXasARFzuWyJEckF?tabId=Sheet1",{
+                    method:'POST',
+                    headers:{
+                            'Content-Type' :'application/json'
+                        },
+                        body: JSON.stringify([[name,email,contact,company,message,new Date().toLocaleString()]])
+                    })
+                    await response.json()
+                    setstate({...state,name:'',email:'',contact:'',company:'',message:''})
+                    setSuccessMessage('Thank You!! We have recieved your query, Look for our email in this next 24 Business Hours')
+                    setTimeout(()=>{
+                            setSuccessMessage('')
+                    window.open( "https://blockreate.com/Docs/Blockreate%20Pitch%20Deck.pdf", "_blank");
+
+                        },3000)
+                }catch (err){
+                    console.log(err)
+                }
+            }
   }
 
 
@@ -59,6 +83,9 @@ const Contact = () => {
             <button className="submit_btn" onClick={submitData}><span class="mdi mdi-spin"/>Submit</button>
             {successMessage &&
                 <h3 className="success__message">{successMessage}</h3>
+            }
+            {errorMessage &&
+                <h3 className="error__message">{errorMessage}</h3>
             }
             </div>
         </div>
